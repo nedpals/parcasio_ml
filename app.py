10444f652b8e1c2315f85b634c5713d663e9380c
Ned <7358345+nedpals@users.noreply.github.com>
2022-12-16 22:27:32 +0000

    initial

--- app.py
+++ app.py
@@ -1,31 +1,37 @@
 from flask import Flask, render_template, request
+from tensorflow import keras
 import csv
 import io
+import numpy as np
+
+model = keras.models.load_model('model.h5')
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route('/')
-def index(name=None):
+def index():
     return render_template("index.html")
 
 @app.post('/result')
 def result():
-    data = request.form
-    b_pressure = request.form.get('Bpressure')
-    fever = request.form.get('Fever')
-    diabetes = request.form.get('Diabetes')
-    vomit = request.form.get('Vomit')
-    classifier = request.form.get('classifier')
+    age = int(request.form.get('Age'))
+    sex = int(request.form.get('Sex'))
+    bp = int(request.form.get('BP'))
+    cholesterol = int(request.form.get('Cholesterol'))
+    natok = int(request.form.get('NaToK'))
     
-    test_input = request.files.get('test_input_file')
-    test_input.stream.seek(0)
-    test_stream = io.StringIO(test_input.stream.read().decode('UTF8'))
-    csv_reader = csv.DictReader(test_stream, delimiter=',', quotechar='"')
-    for row in csv_reader:
-        b_pressure = row['blood pressure']
-        fever = row['fever']
-        diabetes = row['diabetes']
-        vomit = row['vomit']
+    # test_input = request.files.get('test_input_file')
+    # test_input.stream.seek(0)
+    # test_stream = io.StringIO(test_input.stream.read().decode('UTF8'))
+    # csv_reader = csv.DictReader(test_stream, delimiter=',', quotechar='"')
+    # for row in csv_reader:
+    #     b_pressure = row['blood pressure']
+    #     fever = row['fever']
+    #     diabetes = row['diabetes']
+    #     vomit = row['vomit']
 
-    return f'{b_pressure} {fever} {diabetes} {vomit} {classifier}'
+    inputs = np.expand_dims(np.array([age, sex, bp, cholesterol, natok]), axis=0)
+    prediction = model.predict(inputs)
+
+    return f'{prediction}'
